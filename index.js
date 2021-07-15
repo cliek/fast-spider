518e6ca97faf89b534b0b68f42b7af8d7c4b8c7d
chenliming <363428643@qq.com>
2021-07-15 19:25:59 +0800

    新增flow模式及测试用例

--- index.js
+++ index.js
@@ -1,15 +1,71 @@
-import qm from './lib/queue/index'
+import { sizeQM } from './lib/queue'
+import { addTask } from './lib/task'
+import C from './lib/consumer'
 // import './config'
 // import { addTask, removeTask } from './lib/task'
-qm.size()
+// qm.size()
 // export default {
 //     addTask,
 //     removeTask
 // }
-for(let i = 1; i< 1000000; i ++){
-    qm.add(String(i), i);
-    if(i % 20 == 0){
-        qm.pop(String(i));
-    }
-}
-console.log();
+
+// example qm
+// console.time('qm')
+// for(let i = 1; i< 2000000; i ++){
+//     qm.add(String(i), i);
+//     if(i % 20 == 0){
+//         qm.pop(String(i));
+//     }
+// }
+// console.timeEnd('qm');
+
+// example flow Tasks
+// addTask('A', function(next, arg){
+//     console.log("A", arg)
+//     next('B', arg+8)
+// })
+
+// addTask('B', async function(next, arg){
+//     console.log("B", arg)
+//     for (let index = 0; index < new Array(arg).fill('x').length; index++) {
+//         const r = await next('C', arg - index);
+//         console.log(index)
+//     }
+// })
+
+// addTask('C', function(next, arg){
+//     return new Promise((resolve, reject) =>{
+//         setTimeout(() => {
+//             console.log("C", arg)
+//             resolve(true)
+//         }, arg);
+//     })
+//     // next()
+// })
+
+// C.start('A',1);
+
+// example sync Tasks
+addTask('A', function(next, arg){
+    new Array(arg.num).fill('x').map((s,i)=>{
+        next('B', {
+            num: i+1
+        })
+    })
+    console.log(sizeQM());
+})
+
+addTask('B', function(next, arg){
+    console.log("b", arg)
+})
+
+addTask('C', function(next, arg){
+    
+})
+
+C.config({
+    interval: 3000,
+    mode: "sync"
+}).start('A',{
+    num: 10
+});
