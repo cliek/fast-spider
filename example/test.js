6f57899796bbd5d66ed8c7fd9849b92768d08ba3
chenliming <363428643@qq.com>
2021-07-29 18:43:14 +0800

    新增导出文件示例

--- example/test.js
+++ example/test.js
@@ -3,7 +3,12 @@ import Comsumer from '../lib/core'
 const C = new Comsumer({
     ThreadsNum: 1,
     interval: 100,
-    taskPath: 'example/task/index'
+    taskPath: 'example/task/index',
+    pipe: {
+        disabled: false,
+        type: 'json',
+        dirPath: 'example/file'
+    }
 });
 
 C.start({
