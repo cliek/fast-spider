43fc37ddd51f666b429ac85260f94f3475d810f5
chenliming <363428643@qq.com>
2021-08-06 10:54:49 +0800

    1.需要配置文件 2.发布到npm

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    // spider mode || 'sync' | 'flow'
-    mode: 'sync', 
+    // spider mode || 'async' | 'flow'
+    mode: 'async', 
     // 当前分发给子线程任务的频率
     interval: 1000,
     // 自定义task文件路径，默认是项目根目录下task目录
@@ -8,7 +8,7 @@ module.exports = {
     // 自定义链表路径，如果需要自己实现链表，请指定该路径，否则请忽略
     // queuePath: '',
     // 使用子线程个数，不指定或者false默认1个线程，如果为true则根据当前计算机自动计算
-    ThreadsNum: 2,
+    ThreadsNum: 3,
     // 定时执行，如果为false或不指定将立即执行，如果需要定时执行请参考 https://github.com/node-schedule/node-schedule 中配置即可
     // schedule: false,
     // 公共函数注入，在回调函数中modules.plugins.XXX 使用
